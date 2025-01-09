f5f4e60153a90edf148aab2112b714658836f057
🖤𝓐𝓛𝓨𝓧🖤 <179976644+alyxshang@users.noreply.github.com>
2025-01-09 11:29:12 +0100

    Update mandy-macos.sh

--- scripts/installers/mandy/mandy-macos.sh
+++ scripts/installers/mandy/mandy-macos.sh
@@ -2,11 +2,11 @@
 
 echo "Fetching Mandy binary..."
 if [[ $(uname -a | grep "x86_64") == "x86_64" ]]; then
-	curl https://github.com/alyxshang/mandy/releases/download/v.0.1.0/mandy-macos-x64-v.0.1.0
+	curl -O https://github.com/alyxshang/mandy/releases/download/v.0.1.0/mandy-macos-x64-v.0.1.0
 	echo "Installing binary..."
 	mv mandy-macos-x64-v.0.1.0 /usr/local/bin/mandy
 else
-	curl https://github.com/alyxshang/mandy/releases/download/v.0.1.0/mandy-macos-aarch64-v.0.1.0
+	curl -O https://github.com/alyxshang/mandy/releases/download/v.0.1.0/mandy-macos-aarch64-v.0.1.0
 	echo "Installing binary..."
 	mv mandy-macos-aarch64-v.0.1.0 /usr/local/bin/mandy
 fi
