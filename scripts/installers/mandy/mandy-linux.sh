27c82299628266c02fffa79fbb7f44cbc1f13250
🖤𝓐𝓛𝓨𝓧🖤 <179976644+alyxshang@users.noreply.github.com>
2025-01-09 09:46:29 +0100

    Add files via upload

---
+++ scripts/installers/mandy/mandy-linux.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Fetching Mandy binary..."
+curl https://github.com/alyxshang/mandy/releases/download/v.0.1.0/mandy-linux-v.0.1.0
+echo "Installing binary..."
+mv mandy-linux-v.0.1.0 /usr/local/bin/mandy
+echo "Testing installation..."
+mandy -v
